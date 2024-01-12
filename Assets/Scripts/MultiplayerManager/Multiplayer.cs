5ff610ab08e80334d5348d52792ef6715aeca8f8
AngelPalomares <64100727+AngelPalomares@users.noreply.github.com>
2024-01-12 08:22:09 -0600

    Update
    Created the multiplayer script to allow players to create the room

--- Assets/Scripts/MultiplayerManager/Multiplayer.cs
+++ Assets/Scripts/MultiplayerManager/Multiplayer.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
 
-public class Multiplayer : MonoBehaviour
+public class Multiplayer : MonoBehaviourPunCallbacks
 {
+    public TMP_InputField CreateInput, JoinINput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CreatetheRoom()
+    {
+        RoomOptions roomoptions = new RoomOptions();
+        roomoptions.MaxPlayers = 2;
+        PhotonNetwork.CreateRoom(CreateInput.text, roomoptions);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(JoinINput.text);
+        
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Main Game");
     }
 }
